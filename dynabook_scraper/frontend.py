563395143a8047ecc3ff0ce5db2551fae4fec2b6
Davide Depau <davide@depau.eu>
2025-01-05 00:18:27 +0100

    Add front-end

---
+++ dynabook_scraper/frontend.py
@@ -0,0 +1,39 @@
+import sys
+
+import jinja2
+from jinja2 import select_autoescape
+
+from dynabook_scraper.utils.paths import data_dir
+
+env = jinja2.Environment(
+    loader=jinja2.PackageLoader("dynabook_scraper"),
+    autoescape=select_autoescape(),
+)
+
+
+def render(name: str, **kwargs) -> str:
+    template = env.get_template(name)
+    return template.render(**kwargs)
+
+
+def cli_build_frontend():
+    base_url = "/"
+    if len(sys.argv) > 1:
+        base_url = sys.argv[1]
+
+    env.globals["base_url"] = base_url.rstrip("/")
+
+    with open(data_dir / "index.html", "w") as f:
+        f.write(render("home.html"))
+
+    (data_dir / "product").mkdir(exist_ok=True)
+    with open(data_dir / "product" / "index.html", "w") as f:
+        f.write(render("product.html"))
+
+    (data_dir / "content").mkdir(exist_ok=True)
+    with open(data_dir / "content" / "index.html", "w") as f:
+        f.write(render("content.html"))
+
+
+if __name__ == "__main__":
+    cli_build_frontend()
