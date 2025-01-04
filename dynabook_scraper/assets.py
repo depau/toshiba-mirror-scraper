456b0666184fdd1c31055f3135aa121181a0987f
Davide Depau <davide@depau.eu>
2025-01-04 23:42:26 +0100

    Fix asset retrieval

--- dynabook_scraper/assets.py
+++ dynabook_scraper/assets.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 
 from dynabook_scraper.utils.common import run_concurrently, download_file
 from .utils import json
-from .utils.paths import assets_dir, products_work_dir
+from .utils.paths import assets_dir, products_work_dir, data_dir
 from .utils.uvloop import async_run
 
 
@@ -20,29 +20,36 @@ async def download_asset(path: str):
     except aiohttp.ClientResponseError as e:
         if e.status == 404:
             tqdm.write(f"Asset not found: {repr(path)}")
+        elif e.status == 403:
+            tqdm.write(f"Access denied: {repr(path)}")
         else:
             raise
 
 
 async def scrape_assets():
-    async with aiofiles.open("data/all_products.json") as f:
+    async with aiofiles.open(data_dir / "all_products.json") as f:
         all_products = await json.aload(f)
 
-    assets = []
+    assets = set()
     for pid, product_type in all_products.items():
         if "pimg" in product_type and product_type["pimg"]:
-            assets.append(product_type["pimg"])
+            assets.add(product_type["pimg"])
 
         for family in product_type["family"]:
             if "fimg" in family and family["fimg"]:
-                assets.append(family["fimg"])
+                assets.add(family["fimg"])
 
-    for file in products_work_dir.glob("model_img.txt"):
+    for file in products_work_dir.glob("*/model_img.txt"):
         async with aiofiles.open(file) as f:
-            assets.append((await f.read()).strip())
+            assets.add((await f.read()).strip())
 
     progress = tqdm(assets, desc="Scraping assets")
-    await run_concurrently(30, download_asset, progress)
+
+    async def coro(path):
+        await download_asset(path)
+        progress.update()
+
+    await run_concurrently(30, coro, assets)
 
 
 def cli_scrape_assets():
